4b923fd62d631fb2b634304ac6cf5fae5c24993e
Ubuntu <ubuntu@ip-172-31-86-85.ec2.internal>
2022-03-28 15:11:21 +0000

    add photo

--- app.py
+++ app.py
@@ -27,6 +27,95 @@ DB_NAME='photos'
 #DB_PASSWORD = 'CloudyWithAChance422'
 #DB_NAME='photogallery'
 
+@app.route('/add', methods=['GET', 'POST'])
+def add_photo():
+    if request.method == 'POST':    
+        uploadedFileURL=''
+        file = request.files['imagefile']
+        title = request.form['title']
+        tags = request.form['tags']
+        description = request.form['description']
+
+        print(title,tags,description)
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            filenameWithPath = os.path.join(UPLOAD_FOLDER, filename)
+            print(filenameWithPath)
+            file.save(filenameWithPath)            
+            uploadedFileURL = s3uploading(filename, filenameWithPath);
+            ExifData=getExifData(filenameWithPath)
+            print(ExifData)
+            ts=time.time()
+            timestamp = datetime.datetime.\
+                        fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+            conn = MySQLdb.connect (host = DB_HOSTNAME,
+                        user = DB_USERNAME,
+                        passwd = DB_PASSWORD,
+                        db = DB_NAME, 
+            port = 3306)
+            cursor = conn.cursor ()
+
+            statement = "INSERT INTO photos.photo \
+                        (CreationTime,Title,Description,Tags,URL,EXIF) \
+                        VALUES ("+\
+                        "'"+str(timestamp)+"', '"+\
+                        title+"', '"+\
+                        description+"', '"+\
+                        tags+"', '"+\
+                        uploadedFileURL+"', '"+\
+                        json.dumps(ExifData)+"');"
+            
+            print(statement)
+            result = cursor.execute(statement)
+            conn.commit()
+            conn.close()
+
+        return redirect('/')
+    else:
+        return render_template('form.html')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+def getExifData(path_name):
+    f = open(path_name, 'rb')
+    tags = exifread.process_file(f)
+    ExifData={}
+    for tag in tags.keys():
+        if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 
+                       'Filename', 'EXIF MakerNote'):
+            key="%s"%(tag)
+            val="%s"%(tags[tag])
+            ExifData[key]=val
+    return ExifData
+
+def s3uploading(filename, filenameWithPath):
+    s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY,
+                            aws_secret_access_key=AWS_SECRET_KEY)
+                       
+    bucket = BUCKET_NAME
+    path_filename = "photos/" + filename
+    print(path_filename)
+    s3.upload_file(filenameWithPath, bucket, path_filename)  
+    s3.put_object_acl(ACL='public-read', 
+                Bucket=bucket, Key=path_filename)
+
+    return "http://"+BUCKET_NAME+\
+            ".s3-website-us-east-1.amazonaws.com/"+ path_filename 
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
     conn = MySQLdb.connect (host = DB_HOSTNAME,
@@ -35,7 +124,7 @@ def home_page():
                         db = DB_NAME, 
             port = 3306)
     cursor = conn.cursor ()
-    cursor.execute("SELECT * FROM photogallery.photogallery2;")
+    cursor.execute("SELECT * FROM photos.photo;")
     results = cursor.fetchall()
     
     items=[]
