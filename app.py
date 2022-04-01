11011a53b224be51b6b4b9171d29f5505314ed94
Brian Tran <BrianTran9832@gmail.com>
2022-03-31 23:38:12 -0500

    Added error handling

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ DB_NAME='photos'
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_photo():
-    if request.method == 'POST':    
+    if request.method == 'POST':
         uploadedFileURL=''
         file = request.files['imagefile']
         title = request.form['title']
@@ -41,7 +41,7 @@ def add_photo():
             filename = file.filename
             filenameWithPath = os.path.join(UPLOAD_FOLDER, filename)
             print(filenameWithPath)
-            file.save(filenameWithPath)            
+            file.save(filenameWithPath)
             uploadedFileURL = s3uploading(filename, filenameWithPath);
             ExifData=getExifData(filenameWithPath)
             print(ExifData)
@@ -52,7 +52,7 @@ def add_photo():
             conn = MySQLdb.connect (host = DB_HOSTNAME,
                         user = DB_USERNAME,
                         passwd = DB_PASSWORD,
-                        db = DB_NAME, 
+                        db = DB_NAME,
             port = 3306)
             cursor = conn.cursor ()
 
@@ -65,7 +65,7 @@ def add_photo():
                         tags+"', '"+\
                         uploadedFileURL+"', '"+\
                         json.dumps(ExifData)+"');"
-            
+
             print(statement)
             result = cursor.execute(statement)
             conn.commit()
@@ -77,11 +77,11 @@ def add_photo():
 
 
 @app.route('/<int:photoID>', methods=['GET'])
-def view_photo(photoID):    
+def view_photo(photoID):
     conn = MySQLdb.connect (host = DB_HOSTNAME,
                         user = DB_USERNAME,
                         passwd = DB_PASSWORD,
-                        db = DB_NAME, 
+                        db = DB_NAME,
             port = 3306)
     cursor = conn.cursor ()
 
@@ -101,11 +101,11 @@ def view_photo(photoID):
         photo['URL'] = item[5]
         photo['ExifData']=json.loads(item[6])
         items.append(photo)
-    conn.close()        
+    conn.close()
     tags=items[0]['Tags'].split(',')
     exifdata=items[0]['ExifData']
-    
-    return render_template('photodetail.html', photo=items[0], 
+
+    return render_template('photodetail.html', photo=items[0],
                             tags=tags, exifdata=exifdata)
 
 
@@ -118,6 +118,9 @@ def allowed_file(filename):
 def bad_request(error):
     return make_response(jsonify({'error': 'Bad request'}), 400)
 
+@app.errorhandler(415)
+def unsupported_media(error):
+    return make_response(jsonify({'error': "Unsupported Media Type"}), 415)
 
 @app.errorhandler(404)
 def not_found(error):
@@ -128,7 +131,7 @@ def getExifData(path_name):
     tags = exifread.process_file(f)
     ExifData={}
     for tag in tags.keys():
-        if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 
+        if tag not in ('JPEGThumbnail', 'TIFFThumbnail',
                        'Filename', 'EXIF MakerNote'):
             key="%s"%(tag)
             val="%s"%(tags[tag])
@@ -138,16 +141,16 @@ def getExifData(path_name):
 def s3uploading(filename, filenameWithPath):
     s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY,
                             aws_secret_access_key=AWS_SECRET_KEY)
-                       
+
     bucket = BUCKET_NAME
     path_filename = "photos/" + filename
     print(path_filename)
-    s3.upload_file(filenameWithPath, bucket, path_filename)  
-    s3.put_object_acl(ACL='public-read', 
+    s3.upload_file(filenameWithPath, bucket, path_filename)
+    s3.put_object_acl(ACL='public-read',
                 Bucket=bucket, Key=path_filename)
 
     return "http://"+BUCKET_NAME+\
-            ".s3-website-us-east-1.amazonaws.com/"+ path_filename 
+            ".s3-website-us-east-1.amazonaws.com/"+ path_filename
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -155,12 +158,12 @@ def home_page():
     conn = MySQLdb.connect (host = DB_HOSTNAME,
                         user = DB_USERNAME,
                         passwd = DB_PASSWORD,
-                        db = DB_NAME, 
+                        db = DB_NAME,
             port = 3306)
     cursor = conn.cursor ()
     cursor.execute("SELECT * FROM photos.photo;")
     results = cursor.fetchall()
-    
+
     items=[]
     for item in results:
         photo={}
@@ -171,7 +174,7 @@ def home_page():
         photo['Tags'] = item[4]
         photo['URL'] = item[5]
         items.append(photo)
-    conn.close()        
+    conn.close()
     print(items)
     return render_template('index.html', photos=items)
 
